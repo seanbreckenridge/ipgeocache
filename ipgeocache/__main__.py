f2e513261434c7713b1eae60dd092b8ca1eef024
serkanozer <serkan@sellercrowd.com>
2020-10-03 15:08:33 +0300

    Use "click" library to improve CLI experience Show a red error message when IPINFO_TOKEN is not set Add some documentation Allow optionally printing the geolocation information as json

--- ipgeocache/__main__.py
+++ ipgeocache/__main__.py
@@ -1,21 +1,29 @@
-import os
-import sys
+import click
 
+from .printers import default_printer, json_printer
 from . import get as get_ip_info
 
 
-def main():
-    assert (
-        "IPINFO_TOKEN" in os.environ
-    ), "Couldn't find the IPINFO_TOKEN environment variable!"
-    if len(sys.argv) <= 1:
-        print(
-            "Must provide IP address to geolocate as the first argument!",
-            file=sys.stderr,
-        )
-        sys.exit(1)
-    for k, v in get_ip_info(sys.argv[1]).items():
-        print(f"{k}={v}")
+@click.command()
+@click.option("--ipinfo-token",
+              help="Authentication token to use ipinfo API. Consult https://ipinfo.io/signup", envvar="IPINFO_TOKEN")
+@click.option("--json", is_flag=True)
+@click.argument("ip")
+def main(ip, ipinfo_token, json):
+    """Gets geolocation for an IP
+
+    IP is public internet protocol address to be geolocated
+    """
+    if ipinfo_token is None:
+        click.secho("Couldn't find the IPINFO_TOKEN environment variable!\n"
+                    "Set it on IPINFO_TOKEN or pass from CLI --ipinfo-token", fg="red")
+        return
+
+    printer = default_printer
+    if json:
+        printer = json_printer
+
+    printer(get_ip_info(ip))
 
 
 if __name__ == "__main__":
