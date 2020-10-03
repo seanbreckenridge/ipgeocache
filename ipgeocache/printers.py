1f6eda8328af2b9ccad2431d6a0f483702c1139c
Sean Breckenridge <seanbrecke@gmail.com>
2020-10-03 05:49:53 -0700

    add typing to printer module

--- ipgeocache/printers.py
+++ ipgeocache/printers.py
@@ -1,10 +1,11 @@
 import json
 
+from . import Json
 
-def default_printer(data):
+def default_printer(data: Json):
     for k, v in data.items():
         print(f"{k}={v}")
 
 
-def json_printer(data):
+def json_printer(data: Json):
     print(json.dumps(data, indent=4, sort_keys=True))
