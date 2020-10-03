3e5f1582f4a94675d9b372fac46c88039bf1ca1a
Sean Breckenridge <seanbrecke@gmail.com>
2020-10-03 05:49:01 -0700

    update docstring to have an example, autoformat

--- ipgeocache/__main__.py
+++ ipgeocache/__main__.py
@@ -1,22 +1,29 @@
 import click
 
-from .printers import default_printer, json_printer
 from . import get as get_ip_info
+from .printers import default_printer, json_printer
 
 
 @click.command()
-@click.option("--ipinfo-token",
-              help="Authentication token to use ipinfo API. Consult https://ipinfo.io/signup", envvar="IPINFO_TOKEN")
+@click.option(
+    "--ipinfo-token",
+    help="Authentication token to use ipinfo API. Consult https://ipinfo.io/signup",
+    envvar="IPINFO_TOKEN",
+)
 @click.option("--json", is_flag=True)
 @click.argument("ip")
 def main(ip, ipinfo_token, json):
-    """Gets geolocation for an IP
+    """
+    Gets geolocation information for an IP address
 
-    IP is public internet protocol address to be geolocated
+    For example: 'ipgeocache 192.30.255.112'
     """
     if ipinfo_token is None:
-        click.secho("Could not find ipinfo access token!\n"
-                    "Set the IPINFO_TOKEN environment variable or pass the --ipinfo-token flag", fg="red")
+        click.secho(
+            "Could not find ipinfo access token!\n"
+            "Set the IPINFO_TOKEN environment variable or pass the --ipinfo-token flag",
+            fg="red",
+        )
         return
 
     printer = default_printer
